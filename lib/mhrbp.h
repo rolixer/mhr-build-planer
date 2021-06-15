b8fdbf7b1d47493a6a50fa2e9ed9cfc45e439335
Roland Rządkowski <roland.rzadkowski@gmail.com>
2021-06-15 10:55:06 +0200

    basic armor and deco structs

--- lib/mhrbp.h
+++ lib/mhrbp.h
@@ -8,6 +8,35 @@
 
 namespace MHRBP {
 
+     struct Armour {
+        int id;
+        std::string name;
+        std::string slot;
+        int slot1;
+        int slot2;
+        int slot3;
+        int defence;
+        int fire_res;
+        int water_res;
+        int ice_res;
+        int lightning_res;
+        int dragon_res;
+        std::string skill1;
+        int skill1_l;
+        std::string skill2;
+        int skill1_2;
+        std::string skill3;
+        int skill1_3;
+        std::string skill4;
+        int skill1_4;
+    };
+
+     struct Decoration {
+         int id;
+         std::string skill;
+         int skill_l;
+     };
+
 
 std::string helloWorld();
 
